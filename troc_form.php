a2139f0d7d310b8b109ea804b1a72a69ac9be3ad
etiennes0904 <etienne.sautivet09@gmail.com>
2023-06-16 08:00:17 +0000

    Premier Commit

--- troc_form.php
+++ troc_form.php
@@ -1,27 +1,21 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>MMitinéraire</title>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="images/green.svg"/>
-    <link rel="stylesheet" type ="text/css" href="style.css">
+<title>MMitinéraire</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type ="text/css" href="style.css">
+<link rel="shortcut icon" type="image/png" href="images/green.svg"/>
 </head>
 <body>
+
 <?php
-// Appel du bloc Header et du Menu
-require 'header.php';
+// Appel du bloc Header et du Menu>
+require ('header.php');
 ?>
 
 <main>
-    <br>
-    <br>
-    <h1>Votre réservation à été prise en compte.</h1>
-    <br>
-    <h3> Proposez maintenant un service en échange du co-voiturage pour finaliser la transaction.</h3>
-    <br>
-      <!-- Contenu principal -->
-      <style>
+    <!-- Contenu principal -->
+    <style>
     body {
       font-family: Arial, sans-serif;
       background-color: #f2f2f2;
@@ -95,13 +89,11 @@ require 'header.php';
 <body>
   <form id="formcontact" action="traitements/envoie_mail2.php" method="post">
     <h2>Proposer un service contre ce trajet</h2>
-    <br>
     <div id='entete'>
       <div>
         <label for="prenom">Prénom<span>*</span></label>
         <input type="text" name="prenom" id="prenom" required>
       </div>
-
       <div>
         <label for="nom">Nom<span>*</span></label>
         <input type="text" name="nom" id="nom" required>
@@ -115,51 +107,14 @@ require 'header.php';
       <label for="message">Service<span>*</span></label>
       <textarea name="message" placeholder="Votre message" required></textarea>
     </div>
-    <center><input type="submit" value="Envoyer"></center>
+    <input type="submit" value="Envoyer">
   </form>
-<?php
-
-require_once 'admin/lib.inc.php';
-
-// Vérifiez si l'utilisateur est connecté
-if (isset($_SESSION['id_utilisateur'])) {
-    // Vérifiez si l'ID du trajet a été transmis
-    if (isset($_GET['id_trajet'])) {
-        $id_trajet = $_GET['id_trajet'];
-        $id_utilisateur = $_SESSION['id_utilisateur'];
-
-        // Connexion à la base de données
-        $mabd = connexionBD();
-
-        // Effectuer la réservation
-        $reservation_effectuee = reserver_trajet($mabd, $id_trajet, $id_utilisateur);
-
-        if ($reservation_effectuee) {
-            echo "<p>Votre réservation pour le trajet ID $id_trajet a été enregistrée avec succès.</p>";
-            echo '<div><a href="reservation.php">Voir mes réservation</a></div>'."\n";
-
-
-        } else {
-            echo "<p>Une erreur s'est produite lors de l'enregistrement de votre réservation.</p>";
-        }
-
-        // Fermer la connexion à la base de données
-        deconnexionBD($mabd);
-    } else {
-        echo "<p>Erreur : ID du trajet non spécifié.</p>";
-    }
-} else {
-    echo "<p>Erreur : Vous devez être connecté pour effectuer une réservation.</p>";
-}
-?>
-<br>
-<br>
-<center><h2><a href="index.php">Retour à l'accueil</a></h2></center>
 </main>
+
 
 <?php
 // Appel du Pied de Page
-require 'footer.php';
+require ('footer.php');
 ?>
 
 </body>
